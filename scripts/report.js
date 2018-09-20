754a0f2544290a2d859c14cf7f3c4e8c635bc639
Soichiro-Yoshimura <yoshimura@soichiro.org>
2018-09-20 20:08:12 +0900

    Diffデータの作成まで

--- scripts/report.js
+++ scripts/report.js
@@ -11,7 +11,6 @@ fs.mkdir(CHANNELS_LOG,  (err) => {
   }
 });
 
-
 module.exports = (robot) => {
   // 定期実行
   const CronJob = require('cron').CronJob;
@@ -25,13 +24,61 @@ module.exports = (robot) => {
   robot.hear(/ch-map-info>/i, (msg) => {
       const username = msg.message.user.name;
       msg.send('Hello, ' + username);
-      fetchChannelList().then((channels) => {
+      createNumMembersDiff().then((channels) => {
         robot.logger.info(channels);
-      });
+      }).catch(console.error);
   });
 
   /**
-   * チャンネル一覧を取得し、その日のファイルとして上書き保存する
+   * 前日と今日のチャンネル人数のDiffを作成する
+   * @return Promise.<Object[]>
+   */
+  function createNumMembersDiff() {
+    return loadYesterdayChannelList().then((yesterdayChannels) => {
+      const yesterdayMap = new Map();
+      yesterdayChannels.forEach((c) => {
+        yesterdayMap.set(c.id, c);
+      });
+
+      return fetchChannelList().then((todayChannels) => {
+        // Diff配列を作る
+        const diffs = [];
+        todayChannels.forEach((c) => {
+          if (yesterdayMap.has(c.id)) {
+            const yesterdayChannel = yesterdayMap.get(c.id);
+            // チャンネル人数に差があるチャンネルを属性足して追加
+            if(c.num_members !== yesterdayChannel.num_members) {
+              c.diff_num_members = c.num_members - yesterdayChannel.num_members;
+              diffs.push(c);
+            }
+          }
+        });
+        return diffs;
+      });
+    });
+  }
+
+  /**
+   * 前日のログファイルをローカルファイルをより取得する
+   * @return Promise.<Object[]>
+   */
+  function loadYesterdayChannelList() {
+    const yesterday = new Date(new Date().getTime() - (1000 * 60 * 60 * 24));
+    const filename = CHANNELS_LOG + '/' + moment(yesterday).format('YYYY-MM-DD') + '.json';
+    return new Promise((resolve, reject) => {
+      fs.readFile(filename, 'utf-8', (err, data) => {
+        if (err) {
+          reject(err);
+          return;
+        } 
+        return resolve(JSON.parse(data));
+      });
+    });
+  }
+
+  /**
+   * チャンネル一覧をSlackより取得し、その日のファイルとして上書き保存する
+   * @return Promise.<Object[]>
    */
   function fetchChannelList() {
     const filename = CHANNELS_LOG + '/' + moment().format('YYYY-MM-DD') + '.json';
