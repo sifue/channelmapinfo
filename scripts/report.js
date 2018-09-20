e1977fed1990fca1461950b217c1726d02d23207
Soichiro-Yoshimura <yoshimura@soichiro.org>
2018-09-20 18:27:49 +0900

    Slackのチャンネルリストを保存するところまで

--- scripts/report.js
+++ scripts/report.js
@@ -1,7 +1,42 @@
 'use strict';
+const { WebClient } = require('@slack/client');
+const fs = require('fs');
+
 module.exports = (robot) => {
-    robot.hear(/hello>/i, (msg) => {
-        const username = msg.message.user.name;
-        msg.send('Hello, ' + username);
+  // 定期実行
+  const CronJob = require('cron').CronJob;
+  const job = new CronJob('0 * * * * *', () => {
+    robot.logger.info('きたよ');
+    robot.send({ room: '#チャンネルマップ'}, 'きたよ');
+  });
+  job.start();
+
+  // チャットリプライ
+  robot.hear(/ch-map-info>/i, (msg) => {
+      const username = msg.message.user.name;
+      msg.send('Hello, ' + username);
+      fetchChannelList().then((channels) => {
+        robot.logger.info(channels);
+      });
+  });
+
+  /**
+   * チャンネル一覧を取得し、ファイルとして保存する
+   */
+  function fetchChannelList() {
+    const token = process.env.HUBOT_SLACK_TOKEN;
+    const web = new WebClient(token);
+
+    return web.channels.list().then((res) => {
+      return new Promise((resolve, reject) => {
+        fs.writeFile('./channels.json', JSON.stringify(res.channels), (err) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          resolve(res.channels);
+        });
+      });
     });
+  }
 };
