cd2cdcbc4d3c45a6017ad880276fb6c97620a82d
Soichiro-Yoshimura <yoshimura@soichiro.org>
2018-09-20 18:45:55 +0900

    その日のファイルを保存し続けるように修正

--- scripts/report.js
+++ scripts/report.js
@@ -1,6 +1,16 @@
 'use strict';
+const moment = require('moment');
 const { WebClient } = require('@slack/client');
 const fs = require('fs');
+const CHANNELS_LOG = 'channels_log';
+
+// 保存フォルダ作成
+fs.mkdir(CHANNELS_LOG,  (err) => {
+  if(err && err.code !== 'EEXIST') { // すでにディレクトリ存在する以外のエラーの場合
+    console.error(err);
+  }
+});
+
 
 module.exports = (robot) => {
   // 定期実行
@@ -21,15 +31,16 @@ module.exports = (robot) => {
   });
 
   /**
-   * チャンネル一覧を取得し、ファイルとして保存する
+   * チャンネル一覧を取得し、その日のファイルとして上書き保存する
    */
   function fetchChannelList() {
+    const filename = CHANNELS_LOG + '/' + moment().format('YYYY-MM-DD') + '.json';
     const token = process.env.HUBOT_SLACK_TOKEN;
     const web = new WebClient(token);
 
     return web.channels.list().then((res) => {
       return new Promise((resolve, reject) => {
-        fs.writeFile('./channels.json', JSON.stringify(res.channels), (err) => {
+        fs.writeFile(filename, JSON.stringify(res.channels), (err) => {
           if (err) {
             reject(err);
             return;
